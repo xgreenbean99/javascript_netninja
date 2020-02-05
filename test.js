48ded33d469be79a1316da7f58397d882c99db1a
xgreenbean <fuckoff@gmail.com>
2020-02-05 10:09:20 -0800

    logical operators

--- test.js
+++ test.js
@@ -1,9 +1,7 @@
-var youLikeMeat = true;
+var myAge = 25;
 
-if(youLikeMeat= true){
-    document.write("you like meat")
+if (myAge <18 || myAge >30 || myAge === 25){
+document.write("You cant come");
 }else{
-
-    document.write("you hate meat")
-
+    document.write("You can come, you cool dude");
 }
